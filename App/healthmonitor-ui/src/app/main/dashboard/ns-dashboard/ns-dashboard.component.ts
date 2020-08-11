5b5b8138e90aa7be11629ee2a95b12d135c4ff3b
Murat MERCAN <mmercan@outlook.com>
2020-08-11 23:22:02 +1000

    console module removed

--- App/healthmonitor-ui/src/app/main/dashboard/ns-dashboard/ns-dashboard.component.ts
+++ App/healthmonitor-ui/src/app/main/dashboard/ns-dashboard/ns-dashboard.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { DeploymentAppService } from 'app/shared/grpc-services/deployment-app.service';
-import { Console } from 'console';
 import { fuseAnimations } from '@fuse/animations';
 
 @Component({
